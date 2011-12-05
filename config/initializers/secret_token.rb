a01e8429689274a38b2225546aa32de2ca2e2206
Huascar Mamani <h.mamani85@gmail.com>
2011-12-05 16:06:04 -0400

    init skeleton app rails and include frontend in public folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zenlab::Application.config.secret_token = '9fc474de4301bc9c5f002e6043c79cc7a102e90af46eb9d6564a71612abfd9e85e9812eac025d7efe1c1c1bbf2ff907b696b4b4d827a0ed569dce5f27fd5e1b9'
